b2ef00789355256e1da0ba183105653c54eec701
Veronika Grudinina <grudinina.vp@gmail.com>
2017-06-24 14:21:36 +0700

    Написала конвертер арабских в римские

--- task1/roman.php
+++ task1/roman.php
@@ -47,7 +47,134 @@
  * @return string
  */
 function toRoman($number) {
-    // todo ваш код здесь
+    //вычисляем тысячи
+    $m1 = $number / 1000;
+    M($m1);
+    //остаток от тысяч
+    $m2 = $number % 1000;
+        
+    //вычисляем полусотни
+    $d1 = $m2 / 500;
+    D($d1);
+    //вычисляем остаток от полусотни
+    $d2 = $m2 % 500;
+    
+    //вычисляем сотни
+    $c1 = $d2 / 100;
+    C($c1);
+    //вычисляем остаток от сотни
+    $c2 = $d2 % 100;
+    
+    //вычисляем полусотни
+    $l1 = $c2 / 50;
+    L($l1);
+    //вычисляем остаток от полусотни
+    $l2 = $c2 % 50;
+    
+    //вычисляем десятки
+    $x1 = $l2 / 10;
+    X($x1);
+    //вычисляем остаток от десятка
+    $x2 = $l2 % 10;
+    
+    //вычисляем пятерки
+    $v1 = $x2 / 5;
+    V($v1);
+    //вычисляем остаток от пятерки
+    $v2 = $x2 % 5;
+    
+    //вычисляем единицы
+    $j1 = $v2 / 1;
+    J($j1);
+    //вычисляем остаток от единиц
+    $j2 = $v2 % 1;
+}
+function M($number) {
+    $i = 0;
+    while ($i < $number) {
+        echo 'M';
+        $i++;
+    }
+    return;
+}
+function D($number) {
+    if ($number == 4) {
+        echo 'CM';
+        return;
+    }
+    else {
+        echo 'D';
+        return;
+    }
+}
+
+function C($number) {
+    if ($number == 4) {
+        echo 'CD';
+        return;
+    }
+    elseif (($number != 0) && $number < 4) {
+         $i = 0;
+        while ($i < $number) {
+            echo 'C';
+            $i++;
+    }
+        return;
+    }
+    else return;
+}
+
+function L($number) {
+    if ($number == 4) {
+        echo 'XC';
+        return;
+    }
+    else {
+        echo 'L';
+        return;
+    }
+}
+function X($number) {
+    if ($number == 4) {
+        echo 'XL';
+        return;
+    }
+    elseif (($number != 0) && $number < 4) {
+         $i = 0;
+        while ($i < $number) {
+            echo 'X';
+            $i++;
+    }
+        return;
+    }
+    else return;
+}
+
+function V($number) {
+    if ($number == 4) {
+        echo 'IX';
+        return;
+    }
+    else {
+        echo 'V';
+        return;
+    }
+}
+
+function J($number) {
+    if ($number == 4) {
+        echo 'IV';
+        return;
+    }
+    elseif (($number != 0) && $number < 4) {
+         $i = 0;
+        while ($i < $number) {
+            echo 'I';
+            $i++;
+    }
+        return;
+    }
+    else return;
 }
 
 $handle = fopen('php://stdin', 'r');
